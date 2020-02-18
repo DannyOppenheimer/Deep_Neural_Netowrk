6057ac0158b390458eaf504248b28e3f03aedea7
DannyOppenheimer <oppenheimerd1@gmail.com>
2020-02-18 11:16:16 -0500

    Made multiple output neurons possible, but it still breaks for anything other than IN [0], [1] OUT [0], [1]

--- NN.py
+++ NN.py
@@ -1,16 +1,18 @@
 import numpy as np
 import sys
-
+from PIL import Image as im
 
 neurons = []
 loading_signs = ['[|]', '[/]', '[–]', '[\\]']
 
 
 class Brain:
+
     def __init__(self, layers):
 
         self.layers = len(layers)
-        self.learning_rate = 0.1
+        self.learning_rate = 0.001
+        self.cost = 0
 
         loop_num = 0
         percentage_total = 0
@@ -21,7 +23,6 @@ class Brain:
 
         # Create an array with the specified layer size
         for j in range(len(layers)):
-
             loop_num += 1
             print('\r', " ".join((array_lang, loading_signs[loop_num % 4])), end='')
 
@@ -43,7 +44,7 @@ class Brain:
             for n in range(len(neurons[m])):
                 percentage_current += 1
                 print('\r', " ".join((connection_lang, str(int(((percentage_current / percentage_total) * 100))), '%',
-                                      loading_signs[percentage_current % 4])), end='')
+                                      loading_signs[percentage_current % 4])), end='', sep='')
                 if m != len(neurons) - 1:
                     for o in range(len(neurons[m + 1])):
                         Connection(neurons[m][n], neurons[m + 1][o])
@@ -53,54 +54,57 @@ class Brain:
         print('\n')
 
     def feed_forward(self, l, inputs):
-        print("Run number:", l)
 
         # This is our feed-forward step. We are first initializing our input neurons with the input values
         for m in range(len(neurons[0])):
             neurons[0][m].value = inputs[l][m]
+
         # Now we set all the other neurons values
         for n in range(len(neurons)):
-            for o in range(len(neurons[n])):
-                # create a temporary value to add our (W + NVal) + B to
-                temp_value = 0
+            if n != 0:
+                for o in range(len(neurons[n])):
+                    # Create a temporary value to add our (W + NVal) + B to
+                    temp_value = 0
 
-                # for each connection backwards, set neuron's own value to (W + NVal) + B
-                for p in range(len(neurons[n][o].connections_back)):
-                    temp_value += \
-                        (neurons[n][o].connections_back[p][0].value * neurons[n][o].connections_back[p][1].weight) \
-                        + neurons[n][o].bias
-                    neurons[n][o].value = self.sigmoid(temp_value)
+                    # For each connection backwards, set neuron's own value to (W * NVal) + B
+                    for p in range(len(neurons[n][o].connections_back)):
+                        temp_value += neurons[n][o].connections_back[p][0].value * neurons[n][o].connections_back[p][1].weight
+
+                    neurons[n][o].value = self.sigmoid(temp_value + neurons[n][o].bias)
 
     def back_propagate(self, l, outputs):
+        # for a lot of these, we will do a reversed enumerated list in order to back propagate, well, backwards
 
         # now, we can calculate the error of the output
         for q in range(len(neurons[len(neurons) - 1])):
-            neurons[len(neurons) - 1][q].error = outputs[l] - neurons[len(neurons) - 1][q].value
+            neurons[len(neurons) - 1][q].error = float(outputs[l][q]) - float(neurons[len(neurons) - 1][q].value)
 
         # Here we calculate the error of all the hidden layers' neurons
         for r, s in reversed(list(enumerate(neurons))):
             for x in range(len(neurons[r])):
                 if r != len(neurons) - 1 and r != 0:
                     temp_error = 0
+
                     for y in range(len(neurons[r][x].connections_forward)):
-                        temp_error += neurons[r][x].connections_forward[y][1].weight * neurons[r][x].connections_forward[y][0].error
-                    neurons[r][x].error += temp_error
+                        temp_error += (neurons[r][x].connections_forward[y][1].weight * neurons[r][x].connections_forward[y][0].error)
+
+                    neurons[r][x].error = float(temp_error * self.sigmoid_derivative(neurons[r][x].value)) + float(neurons[r][x].error)
 
         # Finally, we calculate and set the weights accordingly
         for t, u in reversed(list(enumerate(neurons))):
             for v in range(len(neurons[t])):
                 for w in range(len(neurons[t][v].connections_forward)):
-                    adjustment_bias = self.learning_rate \
-                                       * (neurons[t][v].connections_forward[w][0].error * self.sigmoid_derivative(neurons[t][v].connections_forward[w][0].value))
 
-                    adjustment_weight = self.learning_rate\
-                                         * ((neurons[t][v].connections_forward[w][0].error * self.sigmoid_derivative(neurons[t][v].connections_forward[w][0].value))
-                                            * neurons[t][v].value)
+                    gradient = self.sigmoid_derivative(neurons[t][v].connections_forward[w][0].value)
+                    error_forward = neurons[t][v].connections_forward[w][0].error
 
-                    neurons[t][v].connections_forward[w][1].weight += adjustment_weight
-                    neurons[t][v].bias += adjustment_bias
+                    adjustment_bias = ((error_forward * gradient) * self.learning_rate)
 
-    def train(self, inputs, outputs, epochs):
+                    adjustment_weight = (((error_forward * gradient) * neurons[t][v].value) * self.learning_rate)
+                    neurons[t][v].connections_forward[w][1].weight = float(adjustment_weight) + float(neurons[t][v].connections_forward[w][1].weight)
+                    neurons[t][v].bias = float(adjustment_bias) + float(neurons[t][v].bias)
+
+    def train(self, inputs, desired_output, epochs):
 
         # Check if inputs match input layer, and if any are above 1 or below -1
         for i in range(len(inputs)):
@@ -113,29 +117,48 @@ class Brain:
         # Real training time!
         for k in range(epochs):
             print('\n', "Epoch", k)
+
             # For each input, run this loop!
             for l in range(len(inputs)):
 
                 self.feed_forward(l, inputs)
-                print("Actual", inputs[l], ": Guess:", neurons[len(neurons) - 1][0].value)
-                self.back_propagate(l, outputs)
-                print("Input", l, ": error on output:", neurons[len(neurons) - 1][0].error)
+
+                # get all outputs
+                output_neuron_outputs = []
+                for m in neurons[len(neurons) - 1]:
+                    output_neuron_outputs.append(m.value)
+                print("For input: ", inputs[l], ", The output was Actual: ", desired_output[l], " – Guess: ", output_neuron_outputs, sep='')
+                self.back_propagate(l, desired_output)
+
+                # get all output errors
+                output_neuron_error = []
+                for n in neurons[len(neurons) - 1]:
+                    output_neuron_error.append(float(n.error))
+
+                print("Input: ", l + 1, " – error on output: ", output_neuron_error, sep='')
 
     @staticmethod
     def sigmoid(x):
         return 1 / (1 + np.exp(-x))
+        # return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def sigmoid_derivative(x):
         return x * (1 - x)
 
     @staticmethod
-    def calc_cost(desired_output, actual_output):
-        return (actual_output - desired_output) ** 2
-
-    @staticmethod
     def shut_down_model(message):
+        neurons.clear()
         sys.exit(message)
+
+    def predict(self, inputs):
+        for i in range(len(inputs)):
+            self.feed_forward(0, inputs)
+
+        output_neuron_outputs = []
+        for j in range(len(neurons[len(neurons) - 1])):
+            output_neuron_outputs.append(neurons[len(neurons) - 1][j].value)
+        return output_neuron_outputs
 
 
 class Neuron:
@@ -159,25 +182,63 @@ class Neuron:
 
 class Connection:
     def __init__(self, source_neuron, destination_neuron):
-        self.weight = (2 * np.random.random(1) - 1)[0]
 
+        self.weight = (np.random.random(1) - 1)[0]
         source_neuron.add_forward_connection(destination_neuron, self)
         destination_neuron.add_backward_connection(source_neuron, self)
 
-    def set_weight(self, x):
-        self.weight = x
+
+def load_images():
+
+    white1 = np.array(im.open('Images/white1.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    white1 = prep_array(white1, 255)
+    white2 = np.array(im.open('Images/white2.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    white2 = prep_array(white2, 255)
+    white3 = np.array(im.open('Images/white3.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    white3 = prep_array(white3, 255)
+    white4 = np.array(im.open('Images/white4.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    white4 = prep_array(white4, 255)
+    white5 = np.array(im.open('Images/white5.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    white5 = prep_array(white5, 255)
+    black1 = np.array(im.open('Images/black1.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    black1 = prep_array(black1, 255)
+    black2 = np.array(im.open('Images/black2.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    black2 = prep_array(black2, 255)
+    black3 = np.array(im.open('Images/black3.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    black3 = prep_array(black3, 255)
+    black4 = np.array(im.open('Images/black4.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    black4 = prep_array(black4, 255)
+    black5 = np.array(im.open('Images/black5.jpg', 'r').convert('RGB').getdata()).flatten().tolist()
+    black5 = prep_array(black5, 255)
+
+    return [white1, white2, white3, white4, white5, black1, black2, black3, black4, black5]
+
+
+def prep_array(array, divisor):
+    for i in range(len(array)):
+        array[i] = array[i] / divisor
+    return array
 
 
 if __name__ == "__main__":
 
     # Setting a random seed is a good idea,
     # so we can get reproducible results and tweak our layer sizes for experimentation
-    np.random.seed(2)
+    np.random.seed(1)
 
-    brain = Brain([2, 2, 1])
+    brain = Brain([2, 3, 3, 2])
 
-    brain.train([[1, 1], [0, 0]],
+    brain.train([[1, 1], [0, 0], [1, 0], [0, 1]],
+                # TODO: implement multiple neuron output layer
+                [[1, 0], [1, 0], [0, 1], [0, 1]],
 
-                [1, 0],
+                10000)
 
-                5000)
+    to_predict = [0, 0]
+    print('\n', 'Predicting:', to_predict, ":", brain.predict([to_predict]))
+    to_predict = [1, 1]
+    print('\n', 'Predicting:', to_predict, ":", brain.predict([to_predict]))
+    to_predict = [0, 1]
+    print('\n', 'Predicting:', to_predict, ":", brain.predict([to_predict]))
+    to_predict = [1, 0]
+    print('\n', 'Predicting:', to_predict, ":", brain.predict([to_predict]))
